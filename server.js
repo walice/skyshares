0b5e25adf6c25b86a768835a8656df21dfec9bb1
Alice Lépissier <alice@skyshares.org>
2017-12-20 11:26:09 -0800

    Update server.js to make it OpenShift v3 compatible.
    Signed-off-by: Alice Lépissier <alice@skyshares.org>

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ var SkyShares = function() {
      */
     self.setupVariables = function() {
 		//  Set the environment variables we need.
-		self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-		self.port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+		self.ipaddress = '0.0.0.0';
+		self.port      = 8080;
 	
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
