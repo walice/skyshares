b45a423d12d25e59e5c3b729e53b10b65fdd260a
jons <jons@surgerydar.co.uk>
2018-02-28 14:26:16 +0000

    cleaning up repo

--- config/configuration.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	mongo : {
-   		user: 		"admin",
-   		password: 	"r6wWkmkfyItZ",
-   		db: 		"skyshares"
-   	}
-};
-
-//ssh://559d60dae0b8cd2de5000097@skyshares-walice.rhcloud.com/~/git/skyshares.git/
