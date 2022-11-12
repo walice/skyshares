bc23c2413f4abe43615d6722681ae4160485f195
Alice Lepissier <alice.lepissier@gmail.com>
2022-11-11 19:21:36 -0500

    Add more debugging in execution code

--- public/javascripts/skyshares.model.worker.js
+++ public/javascripts/skyshares.model.worker.js
@@ -1055,6 +1055,7 @@ var model = {
 			sendprogress( "finished building model" );
 		} catch( error ) {
 			log( 'error creating COWMac : ' + error );
+			log( 'origin :' + self.scope[ cow_mac ])
 		}
 	},
 
